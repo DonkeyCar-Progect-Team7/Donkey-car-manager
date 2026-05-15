d6787f0d633193558a9d490cab4e237cde66556a
Seop <ttalgee12@gmail.com>
2026-05-15 22:52:20 +0900

    feat. UI 배치 완

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,14 @@
             pictureBox1 = new PictureBox();
             trackBar1 = new TrackBar();
             button1 = new Button();
-            button2 = new Button();
             button3 = new Button();
+            listBox2 = new ListBox();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button4 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
@@ -47,45 +53,105 @@
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(234, 353);
+            trackBar1.Location = new Point(234, 335);
             trackBar1.Name = "trackBar1";
             trackBar1.Size = new Size(668, 45);
             trackBar1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(24, 436);
+            button1.Location = new Point(12, 385);
             button1.Name = "button1";
-            button1.Size = new Size(157, 51);
+            button1.Size = new Size(68, 136);
             button1.TabIndex = 2;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            button2.Location = new Point(24, 550);
-            button2.Name = "button2";
-            button2.Size = new Size(157, 51);
-            button2.TabIndex = 3;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            button3.Location = new Point(24, 493);
+            button3.Location = new Point(962, 7);
             button3.Name = "button3";
             button3.Size = new Size(157, 51);
             button3.TabIndex = 4;
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(86, 385);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(1033, 274);
+            listBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 519);
+            button2.Name = "button2";
+            button2.Size = new Size(68, 136);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(251, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 9;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 10;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 11;
+            label4.Text = "label4";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(962, 64);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 51);
+            button4.TabIndex = 12;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1131, 667);
-            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button2);
+            Controls.Add(listBox2);
+            Controls.Add(button3);
             Controls.Add(button1);
             Controls.Add(trackBar1);
             Controls.Add(pictureBox1);
@@ -102,7 +168,13 @@
         private PictureBox pictureBox1;
         private TrackBar trackBar1;
         private Button button1;
-        private Button button2;
         private Button button3;
+        private ListBox listBox2;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button4;
     }
 }
