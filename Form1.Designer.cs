033f0feaf7d0e674e33d5ff736a2253dc6ae1152
성현 이 <leetjdgus@gmail.com>
2026-05-15 22:46:11 +0900

    ui 기본구성 제작 -1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            trackBar1 = new TrackBar();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(234, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(668, 317);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(234, 353);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(668, 45);
+            trackBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 436);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 51);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(24, 550);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 51);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(24, 493);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 51);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1131, 667);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(trackBar1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TrackBar trackBar1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
