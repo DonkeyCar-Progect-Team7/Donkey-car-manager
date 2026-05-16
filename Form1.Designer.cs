184e719c252e58bab9d400a092fb7a9a312b8a93
Seop <ttalgee12@gmail.com>
2026-05-17 03:22:03 +0900

    feat. UI 기본 구성 배치 완료 -기존 버튼 외에 숫자 입력으로 페이지 및 프레임 이동 버튼

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,153 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            trackBar1 = new TrackBar();
-            button1 = new Button();
-            button3 = new Button();
-            listBox2 = new ListBox();
-            button2 = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            button4 = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            picCurFrame = new PictureBox();
+            trbFrame = new TrackBar();
+            btnPageUp = new Button();
+            btnFileOpen = new Button();
+            lstFiles = new ListBox();
+            btnPageDown = new Button();
+            lblTitle = new Label();
+            lblFrameNum = new Label();
+            lblAcceleration = new Label();
+            lblSteeringAngle = new Label();
+            btnFileDelete = new Button();
+            txbFrame = new TextBox();
+            btnFrameMove = new Button();
+            lblCurFilePage = new Label();
+            txbFileNum = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)picCurFrame).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trbFrame).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
+            // picCurFrame
             // 
-            pictureBox1.Location = new Point(234, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(668, 317);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            picCurFrame.Location = new Point(234, 12);
+            picCurFrame.Name = "picCurFrame";
+            picCurFrame.Size = new Size(668, 317);
+            picCurFrame.TabIndex = 0;
+            picCurFrame.TabStop = false;
             // 
-            // trackBar1
+            // trbFrame
             // 
-            trackBar1.Location = new Point(234, 335);
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(668, 45);
-            trackBar1.TabIndex = 1;
+            trbFrame.Location = new Point(234, 335);
+            trbFrame.Name = "trbFrame";
+            trbFrame.Size = new Size(668, 45);
+            trbFrame.TabIndex = 1;
             // 
-            // button1
+            // btnPageUp
             // 
-            button1.Location = new Point(12, 385);
-            button1.Name = "button1";
-            button1.Size = new Size(68, 136);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnPageUp.Font = new Font("맑은 고딕", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnPageUp.Location = new Point(12, 385);
+            btnPageUp.Name = "btnPageUp";
+            btnPageUp.Size = new Size(68, 116);
+            btnPageUp.TabIndex = 2;
+            btnPageUp.Text = "▲";
+            btnPageUp.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnFileOpen
             // 
-            button3.Location = new Point(962, 7);
-            button3.Name = "button3";
-            button3.Size = new Size(157, 51);
-            button3.TabIndex = 4;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            btnFileOpen.Font = new Font("맑은 고딕", 12F);
+            btnFileOpen.Location = new Point(908, 256);
+            btnFileOpen.Name = "btnFileOpen";
+            btnFileOpen.Size = new Size(211, 51);
+            btnFileOpen.TabIndex = 4;
+            btnFileOpen.Text = "파일 열기";
+            btnFileOpen.UseVisualStyleBackColor = true;
             // 
-            // listBox2
+            // lstFiles
             // 
-            listBox2.FormattingEnabled = true;
-            listBox2.Location = new Point(86, 385);
-            listBox2.Name = "listBox2";
-            listBox2.Size = new Size(1033, 274);
-            listBox2.TabIndex = 6;
+            lstFiles.FormattingEnabled = true;
+            lstFiles.Location = new Point(86, 385);
+            lstFiles.Name = "lstFiles";
+            lstFiles.Size = new Size(1033, 274);
+            lstFiles.TabIndex = 6;
             // 
-            // button2
+            // btnPageDown
             // 
-            button2.Location = new Point(12, 519);
-            button2.Name = "button2";
-            button2.Size = new Size(68, 136);
-            button2.TabIndex = 7;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnPageDown.Font = new Font("맑은 고딕", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnPageDown.Location = new Point(12, 539);
+            btnPageDown.Name = "btnPageDown";
+            btnPageDown.Size = new Size(68, 116);
+            btnPageDown.TabIndex = 7;
+            btnPageDown.Text = "▼";
+            btnPageDown.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblTitle
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(14, 7);
-            label1.Name = "label1";
-            label1.Size = new Size(39, 15);
-            label1.TabIndex = 8;
-            label1.Text = "label1";
-            label1.Click += label1_Click;
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("티웨이_하늘", 11.9999981F, FontStyle.Italic, GraphicsUnit.Point, 129);
+            lblTitle.Location = new Point(12, 12);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(191, 19);
+            lblTitle.TabIndex = 8;
+            lblTitle.Text = "Donkey Car Manager";
+            lblTitle.Click += label1_Click;
             // 
-            // label2
+            // lblFrameNum
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(251, 30);
-            label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 9;
-            label2.Text = "label2";
+            lblFrameNum.AutoSize = true;
+            lblFrameNum.Font = new Font("맑은 고딕", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblFrameNum.Location = new Point(235, 14);
+            lblFrameNum.Name = "lblFrameNum";
+            lblFrameNum.Size = new Size(97, 20);
+            lblFrameNum.TabIndex = 9;
+            lblFrameNum.Text = "프레임 번호 :";
             // 
-            // label3
+            // lblAcceleration
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(14, 147);
-            label3.Name = "label3";
-            label3.Size = new Size(39, 15);
-            label3.TabIndex = 10;
-            label3.Text = "label3";
+            lblAcceleration.AutoSize = true;
+            lblAcceleration.Font = new Font("맑은 고딕", 14.25F);
+            lblAcceleration.Location = new Point(12, 43);
+            lblAcceleration.Name = "lblAcceleration";
+            lblAcceleration.Size = new Size(50, 25);
+            lblAcceleration.TabIndex = 10;
+            lblAcceleration.Text = "속도";
             // 
-            // label4
+            // lblSteeringAngle
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(14, 165);
-            label4.Name = "label4";
-            label4.Size = new Size(39, 15);
-            label4.TabIndex = 11;
-            label4.Text = "label4";
+            lblSteeringAngle.AutoSize = true;
+            lblSteeringAngle.Font = new Font("맑은 고딕", 14.25F);
+            lblSteeringAngle.Location = new Point(12, 68);
+            lblSteeringAngle.Name = "lblSteeringAngle";
+            lblSteeringAngle.Size = new Size(69, 25);
+            lblSteeringAngle.TabIndex = 11;
+            lblSteeringAngle.Text = "조향각";
             // 
-            // button4
+            // btnFileDelete
             // 
-            button4.Location = new Point(962, 64);
-            button4.Name = "button4";
-            button4.Size = new Size(157, 51);
-            button4.TabIndex = 12;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btnFileDelete.Font = new Font("맑은 고딕", 12F);
+            btnFileDelete.Location = new Point(908, 313);
+            btnFileDelete.Name = "btnFileDelete";
+            btnFileDelete.Size = new Size(211, 51);
+            btnFileDelete.TabIndex = 12;
+            btnFileDelete.Text = "파일 삭제";
+            btnFileDelete.UseVisualStyleBackColor = true;
+            // 
+            // txbFrame
+            // 
+            txbFrame.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txbFrame.Location = new Point(908, 12);
+            txbFrame.Name = "txbFrame";
+            txbFrame.Size = new Size(123, 29);
+            txbFrame.TabIndex = 13;
+            // 
+            // btnFrameMove
+            // 
+            btnFrameMove.Font = new Font("맑은 고딕", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnFrameMove.Location = new Point(1037, 12);
+            btnFrameMove.Name = "btnFrameMove";
+            btnFrameMove.Size = new Size(87, 29);
+            btnFrameMove.TabIndex = 14;
+            btnFrameMove.Text = "프레임 이동";
+            btnFrameMove.UseVisualStyleBackColor = true;
+            // 
+            // lblCurFilePage
+            // 
+            lblCurFilePage.AutoSize = true;
+            lblCurFilePage.Location = new Point(86, 365);
+            lblCurFilePage.Name = "lblCurFilePage";
+            lblCurFilePage.Size = new Size(78, 15);
+            lblCurFilePage.TabIndex = 15;
+            lblCurFilePage.Text = "현재 페이지 :";
+            // 
+            // txbFileNum
+            // 
+            txbFileNum.Location = new Point(12, 507);
+            txbFileNum.Name = "txbFileNum";
+            txbFileNum.Size = new Size(68, 23);
+            txbFileNum.TabIndex = 16;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1131, 667);
-            Controls.Add(button4);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button2);
-            Controls.Add(listBox2);
-            Controls.Add(button3);
-            Controls.Add(button1);
-            Controls.Add(trackBar1);
-            Controls.Add(pictureBox1);
+            Controls.Add(txbFileNum);
+            Controls.Add(lblCurFilePage);
+            Controls.Add(btnFrameMove);
+            Controls.Add(txbFrame);
+            Controls.Add(btnFileDelete);
+            Controls.Add(lblSteeringAngle);
+            Controls.Add(lblAcceleration);
+            Controls.Add(lblFrameNum);
+            Controls.Add(lblTitle);
+            Controls.Add(btnPageDown);
+            Controls.Add(lstFiles);
+            Controls.Add(btnFileOpen);
+            Controls.Add(btnPageUp);
+            Controls.Add(trbFrame);
+            Controls.Add(picCurFrame);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picCurFrame).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trbFrame).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
-        private TrackBar trackBar1;
-        private Button button1;
-        private Button button3;
-        private ListBox listBox2;
-        private Button button2;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Button button4;
+        private PictureBox picCurFrame;
+        private TrackBar trbFrame;
+        private Button btnPageUp;
+        private Button btnFileOpen;
+        private ListBox lstFiles;
+        private Button btnPageDown;
+        private Label lblTitle;
+        private Label lblFrameNum;
+        private Label lblAcceleration;
+        private Label lblSteeringAngle;
+        private Button btnFileDelete;
+        private TextBox txbFrame;
+        private Button btnFrameMove;
+        private Label lblCurFilePage;
+        private TextBox txbFileNum;
     }
 }
